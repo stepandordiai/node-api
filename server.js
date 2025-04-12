728772e00c169df20da6a8a01565abd1994f105f
Stepan Dordiai <stepandordiai@gmail.com>
2025-04-12 12:48:37 +0200

    v1 Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const proudctRoute = require("./routes/productRoute");
+const errorMiddleware = require("./middleware/errorMiddleware");
+var cors = require("cors");
+
+const app = express();
+
+// const PORT = process.env.PORT || 3000;
+// const MONGO_URL = process.env.MONGO_URL;
+// const FRONTEND = process.env.FRONTEND;
+
+var corsOptions = {
+    origin: "http://192.168.3.105:5173",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+
+app.use("/api/product", proudctRoute);
+
+app.get("/", (req, res) => {
+    res.send("Hello NODE API");
+});
+
+app.get("/blog", (req, res) => {
+    res.send("Hello Blog, My name is Devtamin");
+});
+
+app.use(errorMiddleware);
+
+mongoose.set("strictQuery", false);
+mongoose
+    .connect(
+        "mongodb+srv://stepandordiaiBdWcumZOkcl:Overwatch1@stepandordiaiapi.1ocur.mongodb.net/NodeAPI?retryWrites=true&w=majority&appName=StepanDordiaiAPI"
+    )
+    .then(() => {
+        console.log("connected to MongoDB");
+        app.listen(3000, () => {
+            console.log(`Node API app is running on port 3000`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
