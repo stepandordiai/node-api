413447c9a8402e477316e7527b02b61ab2616d06
Stepan Dordiai <stepandordiai@gmail.com>
2025-04-12 13:05:30 +0200

    v2

--- models/productModel.js
+++ models/productModel.js
@@ -1,31 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema(
     {
         name: {
             type: String,
-            required: [true, "Please enter a product name"]
+            required: [true, "Please enter some info"],
         },
-        quantity: {
-            type: Number,
-            required: true,
-            default: 0
-        },
-        price: {
-            type: Number,
-            required: true,
-        },
-        image: {
-            type: String,
-            required: false,
-        }
+        // quantity: {
+        //     type: Number,
+        //     required: true,
+        //     default: 0
+        // },
+        // price: {
+        //     type: Number,
+        //     required: true,
+        // },
+        // image: {
+        //     type: String,
+        //     required: false,
+        // }
     },
     {
-        timestamps: true
+        timestamps: true,
     }
-)
+);
 
-
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
